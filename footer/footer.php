596f95a3c47503ea663e88a477da29287e25a60b
simplon-IsabelD <igomesdacosta@gmail.com>
2016-02-01 12:35:33 +0100

    fichier footer

---
+++ footer/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+
+	<link rel="stylesheet" href="footerStyle.css" media="all" type="text/css">
+</head>
+<body>
+</body>
+
+<div class="footer">
+
+<h1 class="titre">Thanks for listening. Now join in.</h1>
+
+<p class="titre2">classSave tracks, follow artists and build playlists. All for free.</p>
+
+<p class="cta">
+<button class="g-opacity-transition bouton bouton-large signupBouton bouton-cta" 
+tabindex="0" title="Create a SoundCloud account">Create account</button>	
+</p>
+</div>
+</html>
